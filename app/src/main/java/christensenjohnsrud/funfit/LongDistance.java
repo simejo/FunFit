2c50e99661d85a5a4403a99f55bbbb7474d0adc2
simejo <simen.johnsrud92@gmail.com>
2016-02-05 12:11:13 -0800

    Initialcommit

---
+++ app/src/main/java/christensenjohnsrud/funfit/LongDistance.java
@@ -0,0 +1,13 @@
+package christensenjohnsrud.funfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LongDistance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_long_distance);
+    }
+}
---
+++ app/src/main/java/christensenjohnsrud/funfit/MainActivity.java
@@ -0,0 +1,42 @@
+package christensenjohnsrud.funfit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btn_interval, btn_long_distance, btn_height_measurement;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_interval = (Button) findViewById(R.id.button_interval);
+        btn_long_distance = (Button) findViewById(R.id.button_long_distance);
+        btn_height_measurement = (Button) findViewById(R.id.button_height_measurement);
+
+        btn_interval.setOnClickListener(this);
+        btn_long_distance.setOnClickListener(this);
+        btn_height_measurement.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_interval){
+            startActivity(new Intent(this, Interval.class));
+        }
+        else if(v.getId() == R.id.button_long_distance){
+            startActivity(new Intent(this, LongDistance.class));
+        }
+        else if(v.getId() == R.id.button_height_measurement){
+            startActivity(new Intent(this, HeightMeasurement.class));
+        }
+    }
+}
