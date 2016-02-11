9f669044d95043816c1c372f78ab973107f4daad
simejo <simen.johnsrud92@gmail.com>
2016-02-11 15:15:25 -0800

    Fixed bug with parsing number

--- app/src/main/java/christensenjohnsrud/funfit/LongDistance.java
+++ app/src/main/java/christensenjohnsrud/funfit/LongDistance.java
@@ -6,25 +6,24 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.annotation.RequiresPermission;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class LongDistance extends AppCompatActivity implements LocationListener, View.OnClickListener, TextWatcher{
 
     LocationManager locationManager;
-    TextView tv_current_speed, tv_speed_threshold;
-    Button btn_plus, btn_minus;
-    private float speed_threshold;
+    TextView tvCurrentSpeed, tvSpeedThreshold;
+    Button btnPlus, btnMinus;
+    private float speedThreshold;
+    private double km_h = 3.6, mph = 2.2369;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +32,7 @@ public class LongDistance extends AppCompatActivity implements LocationListener,
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("INSIDE IF", "_----------__--_-_-_");
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -42,19 +42,20 @@ public class LongDistance extends AppCompatActivity implements LocationListener,
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-        tv_current_speed = (TextView) findViewById(R.id.textView_current_speed);
+        tvCurrentSpeed = (TextView) findViewById(R.id.textView_current_speed);
 
-        speed_threshold = /*Load a value from database*/ 8.0f;
+        speedThreshold = /*Load a value from database*/ 8.0f;
 
-        btn_minus = (Button) findViewById(R.id.button_minus);
-        btn_plus = (Button) findViewById(R.id.button_plus);
+        btnMinus = (Button) findViewById(R.id.button_minus);
+        btnPlus = (Button) findViewById(R.id.button_plus);
 
-        btn_minus.setOnClickListener(this);
-        btn_plus.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+        btnPlus.setOnClickListener(this);
 
-        tv_speed_threshold = (TextView) findViewById(R.id.textView_speed_threshold);
-        tv_speed_threshold.addTextChangedListener(this);
+        tvSpeedThreshold = (TextView) findViewById(R.id.textView_speed_threshold);
+        tvSpeedThreshold.addTextChangedListener(this);
         updateThresholdText();
 
 
@@ -63,42 +64,44 @@ public class LongDistance extends AppCompatActivity implements LocationListener,
     @Override
     public void onLocationChanged(Location location) {
         location.getLatitude();
-        tv_current_speed.setText("Current speed: " + location.getSpeed() + "GPS accuracy" + location.getAccuracy());
-        if(location.getSpeed() < speed_threshold){
-            tv_current_speed.setText("Current speed: " + location.getSpeed() + "GPS accuracy" + location.getAccuracy() + "TOO SLOW");
+        Log.i("onLocationChanged", "called");
+        double speed = location.getSpeed()*km_h; //Convert from m/s to km/h
+        tvCurrentSpeed.setText("Current speed: " + speed + "GPS accuracy" + location.getAccuracy());
+        if(speed < speedThreshold){
+            tvCurrentSpeed.setText("Current speed: " + speed + "GPS accuracy" + location.getAccuracy() + "TOO SLOW");
         }
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-
+        Log.i("onStatusChanged","called");
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-
+        Log.i("onProviderEnabled","called");
     }
 
     @Override
     public void onProviderDisabled(String provider) {
+        Log.i("onProviderDisabled","called");
 
     }
 
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.button_minus){
-            if(speed_threshold > 0.1){
-                speed_threshold -= 0.1;
+            if(speedThreshold > 0.1){
+                speedThreshold -= 0.1;
             }
             else{
-                speed_threshold = 0;
+                speedThreshold = 0;
             }
-            updateThresholdText();
         }
         else if(v.getId() == R.id.button_plus){
-            speed_threshold += 0.1;
-            updateThresholdText();
+            speedThreshold += 0.1;
         }
+        updateThresholdText();
     }
 
     @Override
@@ -113,16 +116,16 @@ public class LongDistance extends AppCompatActivity implements LocationListener,
     @Override
     public void afterTextChanged(Editable s) {
         if(s.toString().equals("")){
-            speed_threshold = 0;
+            speedThreshold = 0;
         }
         else{
-            speed_threshold = Float.parseFloat(s.toString());
+            String num = s.toString().replace(',','.');
+            speedThreshold = Float.valueOf(num);
         }
-        //updateThresholdText();
+
     }
 
     public void updateThresholdText(){
-        tv_speed_threshold.setText(String.format("%.1f", speed_threshold));
-        //tv_speed_threshold.setText(speed_threshold + "");
+        tvSpeedThreshold.setText(String.format("%.1f", speedThreshold));
     }
 }
