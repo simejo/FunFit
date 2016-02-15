5d7f610699ef5b5ef76ac68fe197b83642b4d49c
siljec <siljechristensen92@gmail.com>
2016-02-15 09:47:03 -0800

    added google api in gradle, made a user class without content.

---
+++ app/src/main/java/christensenjohnsrud/funfit/User.java
@@ -0,0 +1,13 @@
+package christensenjohnsrud.funfit;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.ActivityRecognition;
+
+/**
+ * Created by siljechristensen on 11/02/16.
+ */
+public class User {
+    private GoogleApiClient googleApiClient;
+
+
+}
