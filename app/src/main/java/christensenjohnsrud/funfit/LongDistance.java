ce84788b642475a10c98830de0ac27e7b1f4dba6
simejo <simen.johnsrud92@gmail.com>
2016-02-18 15:24:06 -0800

    Added upper boundary to long distance and implemented timer

--- app/src/main/java/christensenjohnsrud/funfit/LongDistance.java
+++ app/src/main/java/christensenjohnsrud/funfit/LongDistance.java
@@ -1,29 +1,35 @@
 package christensenjohnsrud.funfit;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.SystemClock;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputFilter;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class LongDistance extends AppCompatActivity implements LocationListener, View.OnClickListener, TextWatcher{
+public class LongDistance extends AppCompatActivity implements LocationListener, View.OnClickListener{
 
     LocationManager locationManager;
-    TextView tvCurrentSpeed, tvSpeedThreshold;
-    Button btnPlus, btnMinus;
-    private float speedThreshold;
+    TextView tvCurrentSpeed, tvSpeedThresholdLower, tvSpeedThresholdUpper, tvAccuracy, tvTimer;
+    Button btnPlusLower, btnMinusLower, btnPlusUpper, btnMinusUpper, btnTimer;
+    private float speedThresholdLower, speedThresholdUpper;
     private double km_h = 3.6, mph = 2.2369;
+    private Timer timer;
+    private boolean timerOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,21 +48,79 @@ public class LongDistance extends AppCompatActivity implements LocationListener,
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+        if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+        }
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         tvCurrentSpeed = (TextView) findViewById(R.id.textView_current_speed);
+        tvAccuracy = (TextView) findViewById(R.id.textView_accuracy);
+        tvTimer = (TextView) findViewById(R.id.timer_long_distance);
 
-        speedThreshold = /*Load a value from database*/ 8.0f;
+        speedThresholdLower = /*Load a value from database*/ 6.0f;
+        speedThresholdUpper = /*Load a value from database*/ 9.0f;
 
-        btnMinus = (Button) findViewById(R.id.button_minus);
-        btnPlus = (Button) findViewById(R.id.button_plus);
+        btnMinusLower = (Button) findViewById(R.id.button_minus_lower);
+        btnPlusLower = (Button) findViewById(R.id.button_plus_lower);
+        btnMinusUpper = (Button) findViewById(R.id.button_minus_upper);
+        btnPlusUpper = (Button) findViewById(R.id.button_plus_upper);
+        btnTimer = (Button) findViewById(R.id.button_timer_long_distance);
 
-        btnMinus.setOnClickListener(this);
-        btnPlus.setOnClickListener(this);
+        btnMinusLower.setOnClickListener(this);
+        btnPlusLower.setOnClickListener(this);
+        btnMinusUpper.setOnClickListener(this);
+        btnPlusUpper.setOnClickListener(this);
+        btnTimer.setOnClickListener(this);
 
-        tvSpeedThreshold = (TextView) findViewById(R.id.textView_speed_threshold);
-        tvSpeedThreshold.addTextChangedListener(this);
+        tvSpeedThresholdLower = (TextView) findViewById(R.id.textView_speed_threshold_lower);
+        tvSpeedThresholdLower.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.toString().equals("")){
+                    speedThresholdLower = 0;
+                }
+                else {
+                    String num = s.toString().replace(',','.');
+                    speedThresholdLower = Float.valueOf(num);
+                }
+            }
+        });
+        tvSpeedThresholdUpper = (TextView) findViewById(R.id.textView_speed_threshold_upper);
+        tvSpeedThresholdUpper.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.toString().equals("")){
+                    speedThresholdUpper = 0;
+                }
+                else {
+                    String num = s.toString().replace(',','.');
+                    speedThresholdUpper = Float.valueOf(num);
+                }
+            }
+        });
         updateThresholdText();
+
+        timer = new Timer(this, R.id.timer_long_distance);
 
 
     }
@@ -66,66 +130,108 @@ public class LongDistance extends AppCompatActivity implements LocationListener,
         location.getLatitude();
         Log.i("onLocationChanged", "called");
         double speed = location.getSpeed()*km_h; //Convert from m/s to km/h
-        tvCurrentSpeed.setText("Current speed: " + speed + "GPS accuracy" + location.getAccuracy());
-        if(speed < speedThreshold){
-            tvCurrentSpeed.setText("Current speed: " + speed + "GPS accuracy" + location.getAccuracy() + "TOO SLOW");
+        tvAccuracy.setText("Accuracy: " + location.getAccuracy());
+        if(speed < speedThresholdLower){
+            tvCurrentSpeed.setText("Current speed: " + speed + "TOO SLOW");
+        } else if (speed > speedThresholdUpper){
+            tvCurrentSpeed.setText("Current speed: " + speed + "TOO FAST");
+        } else{
+            tvCurrentSpeed.setText("Current speed: " + speed);
         }
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-        Log.i("onStatusChanged","called");
+        tvAccuracy.setText("Accuracy: onStatusChange");
+        Log.i("onStatusChanged", "called");
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-        Log.i("onProviderEnabled","called");
+        tvAccuracy.setText("Accuracy: onProviderEnabled");
+        Log.i("onProviderEnabled", "called");
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-        Log.i("onProviderDisabled","called");
+        tvAccuracy.setText("Accuracy: onProviderDisabled");
+        Log.i("onProviderDisabled", "called");
 
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.button_minus){
-            if(speedThreshold > 0.1){
-                speedThreshold -= 0.1;
+        if(v.getId() == R.id.button_minus_lower){
+            if(speedThresholdLower > 0.1){
+                speedThresholdLower -= 0.1;
             }
             else{
-                speedThreshold = 0;
+                speedThresholdLower = 0;
             }
         }
-        else if(v.getId() == R.id.button_plus){
-            speedThreshold += 0.1;
+        else if(v.getId() == R.id.button_plus_lower){
+            if(speedThresholdLower < speedThresholdUpper){
+                speedThresholdLower += 0.1;
+            }
         }
+
+        else if(v.getId() == R.id.button_minus_upper){
+            if(speedThresholdUpper > 0.1){
+                if(speedThresholdLower < speedThresholdUpper){
+                    speedThresholdUpper -= 0.1;
+                }
+            }
+            else{
+                speedThresholdUpper = 0;
+            }
+        }
+        else if(v.getId() == R.id.button_plus_upper){
+            speedThresholdUpper += 0.1;
+        }
+        else if(v.getId() == R.id.button_timer_long_distance){
+            if(timerOn){
+                timerOn = false;
+                timer.setStartTime(SystemClock.uptimeMillis());
+                timer.removeHandlerCallback();
+                tvTimer.setText(timer.getCurrentTime());
+                timer.postDelayed();
+                btnTimer.setText("Start");
+            }
+            else{
+                timerOn = true;
+
+                timer.setStartTime(SystemClock.uptimeMillis());
+                timer.removeHandlerCallback();
+                tvTimer.setText(timer.getCurrentTime());
+
+                timer.postDelayed();
+                btnTimer.setText("Pause");
+            }
+        }
+
         updateThresholdText();
     }
 
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable s) {
-        if(s.toString().equals("")){
-            speedThreshold = 0;
-        }
-        else{
-            String num = s.toString().replace(',','.');
-            speedThreshold = Float.valueOf(num);
-        }
-
-    }
-
     public void updateThresholdText(){
-        tvSpeedThreshold.setText(String.format("%.1f", speedThreshold));
+        tvSpeedThresholdLower.setText(String.format("%.1f", speedThresholdLower));
+        tvSpeedThresholdUpper.setText(String.format("%.1f", speedThresholdUpper));
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
     }
 }
