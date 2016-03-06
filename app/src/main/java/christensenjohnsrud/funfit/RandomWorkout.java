992b775cf19827e225e1d6360304d51d0bde2ec7
siljec <siljechristensen92@gmail.com>
2016-03-06 02:01:00 -0800

    minor changes

--- app/src/main/java/christensenjohnsrud/funfit/RandomWorkout.java
+++ app/src/main/java/christensenjohnsrud/funfit/RandomWorkout.java
@@ -9,9 +9,6 @@ import android.util.Log;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 /**
@@ -19,22 +16,21 @@ import java.util.Random;
  */
 public class RandomWorkout extends Activity implements SensorEventListener {
 
-
     private String className = "RandomWorkout";
     private static int SHAKE_THRESHOLD = 16;
 
-    // NB! Remember to update both type and types
+    // NB! Remember to update all the lists
     private String[] core = {"Planken i 1 min","Saks i 1 min","60 sit ups"};
     private String[] booty = {"20 Lunges","20 Squat"};
     private String[] arms = {"Weight","Lift"};
     private String[] back = {"rygghev","mark"};
-    private String[][] types = {core,booty, arms, back}; // TYPES
-    private String[] np_types = {"core","booty", "arms", "back"};
+    private String[][] types = {core,booty, arms, back};            // Needed for acceleration
+    private String[] np_types = {"core","booty", "arms", "back"};   // needed for numberpicker
 
     private int chosenType;
 
 
-    private NumberPicker type;
+    private NumberPicker type_picker;
     private TextView exercise_tv;
 
     @Override
@@ -42,35 +38,39 @@ public class RandomWorkout extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_workout);
 
-
         chosenType = 1;
 
-        type = (NumberPicker) findViewById(R.id.type_picker);
-        type.setMinValue(0); //from array first value
+        // NumberPicker
+        type_picker = (NumberPicker) findViewById(R.id.type_picker);
+        type_picker.setMinValue(0); //from array first value
         //Specify the maximum value/number of NumberPicker
-        type.setMaxValue(types.length - 1); //to array last value
-        type.setWrapSelectorWheel(true);
-        type.setDisplayedValues(np_types);
+        type_picker.setMaxValue(types.length - 1); //to array last value
+        type_picker.setWrapSelectorWheel(true);
+        type_picker.setDisplayedValues(np_types);
 
 
         exercise_tv = (TextView) findViewById(R.id.text_view_exercise);
 
-        type.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+        type_picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
                 // TODO Auto-generated method stub
-                Log.i("main", newVal +"");
+                Log.i("main", newVal + "");
                 switch (newVal) {
-                    case 1: exercise_tv.setText(getRandomWorkout(core));
+                    case 1:
+                        exercise_tv.setText(getRandomWorkout(core));
                         chosenType = 1;
                         break;
-                    case 2: exercise_tv.setText(getRandomWorkout(booty));
+                    case 2:
+                        exercise_tv.setText(getRandomWorkout(booty));
                         chosenType = 2;
                         break;
-                    case 3: exercise_tv.setText(getRandomWorkout(arms));
+                    case 3:
+                        exercise_tv.setText(getRandomWorkout(arms));
                         chosenType = 3;
                         break;
-                    case 4: exercise_tv.setText(getRandomWorkout(back));
+                    case 4:
+                        exercise_tv.setText(getRandomWorkout(back));
                         chosenType = 4;
                         break;
                 }
@@ -99,6 +99,5 @@ public class RandomWorkout extends Activity implements SensorEventListener {
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 }
