06f8a527a8e93e6d507694f62fba7e4603125bf7
simejo <simen.johnsrud92@gmail.com>
2016-03-05 18:26:10 -0800

    Added radiobuttons to change unit in longDistance + more textViews to show result

--- app/src/main/java/christensenjohnsrud/funfit/measure.java
+++ app/src/main/java/christensenjohnsrud/funfit/measure.java
@@ -9,21 +9,27 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class measure extends Activity implements OnClickListener {
 	public final static String SHARED_PREFS_NAME="sonarsettings";
     sonicPing sp;
     CheckBox cb;
     AudioManager am;
-    String unit = "m";
+    String unit = "centimeter";
     float SoS = 340.f;
     boolean pinging = false;
     Button button;
 	String TAG = "measure.java";
-	TextView tv_height;
+	TextView tv_height, tv_height1, tv_height2, tv_height3, tv_height4, tv_height5;
+	RadioButton radio_centimeter, radio_inch;
+	private float unitFactor = 100;
+	DecimalFormat format = new DecimalFormat("0.00");
 
-    @Override
+	@Override
     public void onConfigurationChanged(Configuration config) {
     	super.onConfigurationChanged(config);
     }
@@ -34,7 +40,6 @@ public class measure extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
         
-		//Create sp from settings
 		sp = new sonicPing();
         am = ((AudioManager)this.getSystemService(AUDIO_SERVICE));
 
@@ -42,38 +47,54 @@ public class measure extends Activity implements OnClickListener {
         button = (Button)findViewById(R.id.button_send_ping);
         button.setOnClickListener(this);
         cb = (CheckBox)findViewById(R.id.CheckMaxVol);
-		tv_height = (TextView) findViewById(R.id.textView_height_measurement);
+		tv_height1 = (TextView) findViewById(R.id.textView_height_measurement1);
+		tv_height2 = (TextView) findViewById(R.id.textView_height_measurement2);
+		tv_height3 = (TextView) findViewById(R.id.textView_height_measurement3);
+		tv_height4 = (TextView) findViewById(R.id.textView_height_measurement4);
+		tv_height5 = (TextView) findViewById(R.id.textView_height_measurement5);
+
+		radio_centimeter = (RadioButton) findViewById(R.id.radioCentimeter);
+		radio_inch = (RadioButton) findViewById(R.id.radioInch);
+
+		radio_centimeter.setOnClickListener(this);
+		radio_inch.setOnClickListener(this);
+		radio_centimeter.setChecked(true);
     }
 
     
 
     public void onClick(View v) {
-		Log.d(TAG, "onClick)");
-		int oldVolume = -100;
-		if (!pinging && cb.isChecked()) {
-			oldVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
-			am.setStreamVolume(AudioManager.STREAM_MUSIC, am.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+		if(v.getId() == R.id.button_send_ping){
+			Log.d(TAG, "onClick)");
+			int oldVolume = -100;
+			if (!pinging && cb.isChecked()) {
+				oldVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
+				am.setStreamVolume(AudioManager.STREAM_MUSIC, am.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+			}
+
+			float[] res = sp.ping();
+			if (res == null) {
+				tv_height.setText("Please ping again");
+			}
+
+			float[][] results = sp.getDistanceList();
+
+			tv_height1.setText("1. " + format.format(results[0][0]*unitFactor) + " " + unit);
+			tv_height2.setText("2. " + format.format(results[1][0]*unitFactor) + " " + unit);
+			tv_height3.setText("3. " + format.format(results[2][0]*unitFactor) + " " + unit);
+			tv_height4.setText("4. " + format.format(results[3][0]*unitFactor) + " " + unit);
+			tv_height5.setText("5. " + format.format(results[4][0]*unitFactor) + " " + unit);
+
+			if (pinging && oldVolume > -100){
+				am.setStreamVolume(AudioManager.STREAM_MUSIC, oldVolume, 0);
+			}
+		} else if(v.getId() == R.id.radioCentimeter){
+			unit = "meter";
+			unitFactor = 100;
+		} else if (v.getId() == R.id.radioInch){
+			unit = "inches";
+			unitFactor = 39.3701f;
 		}
-
-		float[] res = sp.ping();
-		if (res == null) {
-			tv_height.setText("Please ping again");
-		}
-
-		int counter = 1;
-		String textHolder = "";
-		for (float[] x : sp.getDistanceList()) {
-			Log.d(TAG + " peaks", counter + ": " + x + "    " + ((Float) (Math.round(x[0] * 100) / 100.f)).toString());
-			textHolder += counter + ": " + x[0] + ", ";
-			counter ++;
-		}
-		//float[][] results = sp.getDistanceList();
-		//float final_height = ((Float) (Math.round(results[0][0] * 100) / 100.f));
-		tv_height.setText(textHolder);
-
-
-		if (pinging && oldVolume > -100)
-			am.setStreamVolume(AudioManager.STREAM_MUSIC, oldVolume, 0);
     }
     
     public void onDestroy() {
